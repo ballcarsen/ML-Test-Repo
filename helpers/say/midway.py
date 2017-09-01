adbdf2e4fa8ce255ef88c2a176570ef59e97059c
Carsen Ball <ballcarsen@gmail.com>
2017-09-01 07:48:41 -0600

    added a midway.py to practice commiting

---
+++ helpers/say/midway.py
@@ -0,0 +1,9 @@
+'''
+Created on Sep 1, 2017
+
+@author: Carsen
+'''
+print ("Midway top level code")
+
+def run():
+    print("midway")  
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import helpers.say.hello as hello
+import helpers.say.midway as midway
 import helpers.say.goodbye as goodbye
 import helpers.classFile as classFile
 
 print("running main")
 
 hello.run()
+midway.run()
 goodbye.run()
 
 classInstance = classFile.simpleClass(str(99))
