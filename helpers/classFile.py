b0555c12bd2af96916b33f0418b6970bb168f5b0
Brett <Leasure57@gmail.com>
2017-08-31 18:42:14 -0600

    added a file with a class to practice O.O. python

---
+++ helpers/classFile.py
@@ -0,0 +1,13 @@
+class simpleClass:
+    
+    print("running code inside class")
+    
+    def __init__(self, parameter):
+        print("in class constructor")
+        print("parameter: " + parameter)
+        
+    def classMethod(self):
+        print("in class method")
+        
+
+        
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import helpers.say.hello as hello
 import helpers.say.goodbye as goodbye
+import helpers.classFile as classFile
 
 print("running main")
 
 hello.run()
 goodbye.run()
+
+classInstance = classFile.simpleClass(str(99))
+classInstance.classMethod();
